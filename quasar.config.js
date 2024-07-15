856af85eabece3bbfcbab00bf824fafd39b4d149
wow-xc <wonheelee20081203@gmail.com>
2024-07-16 08:41:37 +0900

    distDir

--- quasar.config.js
+++ quasar.config.js
@@ -66,7 +66,7 @@ module.exports = configure(function (ctx) {
 
       // https://v2.quasar.dev/quasar-cli-webpack/handling-webpack
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-
+      distDir: "dist/spa",
       chainWebpack(chain) {
         chain
           .plugin("eslint-webpack-plugin")
